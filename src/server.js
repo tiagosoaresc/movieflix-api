425acf43e09f13fee6eb0c4734c9d96a36dd2933
Tiago Soares de Carvalho <devtiagosoaresc@gmail.com>
2026-04-14 17:24:46 -0300

    configurações iniciais do projeto

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import http from 'http';
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.end('Home page');
+    }
+    else if (req.url === '/sobre') {
+        res.statusCode = 200;
+        res.end('About page');
+    }
+});
+server.listen(3000, () => {
+    console.log(`Servidor em execução em http://localhost:3000/`);
+});
+//# sourceMappingURL=server.js.map
