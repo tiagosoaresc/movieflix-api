7accd2688bdfce82c5c74ac38310debd15e164e7
Tiago Soares de Carvalho <devtiagosoaresc@gmail.com>
2026-04-14 23:58:00 -0300

    Criando a primeira porta do servidor

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import express from 'express'
 const port = 3000
 const app = express()
 
-app.get("/", (req,res) =>{
-    res.send("Home page")
+app.get("/movies", (req,res) =>{
+    res.send("Listagem de filmes")
 })
 
 app.listen(port, ()=>{
