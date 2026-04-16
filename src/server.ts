56dc90175031091d121f80bd062ddf1ea7e4211d
Tiago Soares de Carvalho <devtiagosoaresc@gmail.com>
2026-04-16 20:40:36 -0300

    Atualização de dados dos filmes

--- src/server.ts
+++ src/server.ts
@@ -24,18 +24,21 @@ app.post('/movies', async (req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } = req.body
 
     try {
-
-        // 1. Verificar no banco se já existe um filme com o mesmo nome que está sendo enviado 
+        // 1. Verificar no banco se já existe um filme com o mesmo nome que está sendo enviado
 
         // 2. Case insensitive - se a busca for feita por letra minúscula ou maiúscula não irá importar, pois será o mesmo nome e o registro vai ser retornado na consulta.
         const movieWithSameTitle = await prisma.movie.findFirst({
             where: {
-                title: {equals: title, mode:"insensitive"}
-            }
+                title: { equals: title, mode: 'insensitive' },
+            },
         })
 
-        if(movieWithSameTitle){
-            return res.status(409).send({message: "Já existe um filme cadastrado com esse título"})
+        if (movieWithSameTitle) {
+            return res
+                .status(409)
+                .send({
+                    message: 'Já existe um filme cadastrado com esse título',
+                })
         }
 
         await prisma.movie.create({
@@ -52,6 +55,47 @@ app.post('/movies', async (req, res) => {
     }
 
     res.status(201).send()
+})
+
+app.put('/movies/:id', async (req, res) => {
+    // Pegar o ID do registro que será atualizado
+    const id = Number(req.params.id)
+
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: {
+                id: id,
+            },
+        })
+
+        if (!movie) {
+            return res
+                .status(404)
+                .send({ message: 'Filme não encontrado, esse ID não existe.' })
+        }
+
+        const data = { ...req.body }
+        // Transformando a data que está em tipo sting em um tipo date, para que possa ser adicionado no banco de dados. Caso n'ao possua uma release_date ela ter o valor undefined e o prisma não irá atualiza-la
+        data.release_date = data.release_date
+            ? new Date(data.release_date)
+            : undefined
+
+        // Pegar os dados do filme que será atualizado e atualizar ele no prisma
+        await prisma.movie.update({
+            where: {
+                id: id,
+            },
+            // Transformar a string da requisição em um tipo date pra conseguir aloca-la no banco de dados
+            data: data,
+        })
+    } catch (error) {
+        return res
+            .status(500)
+            .send({ message: 'Falha ao atualizar o registro do filme.' })
+    }
+
+    // Retornar o status correto informando que o filme foi atualizado
+    res.status(200).send({ message: `O filme com o id: ${id}, foi atualizado` })
 })
 
 app.listen(port, () => {
