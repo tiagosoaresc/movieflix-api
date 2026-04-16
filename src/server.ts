a25f2f7bb4664d9526c8de20ee9f8e19272b95c9
Tiago Soares de Carvalho <devtiagosoaresc@gmail.com>
2026-04-16 09:59:39 -0300

    Cadastrando um filme  teste

--- src/server.ts
+++ src/server.ts
@@ -5,19 +5,32 @@ const port = 3000
 const app = express()
 const prisma = new PrismaClient()
 
-app.get("/movies", async (req,res) =>{
+app.get('/movies', async (req, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
-            title: "asc"
+            title: 'asc',
         },
         include: {
-            genres:true,
-            languages:true
-        }
+            genres: true,
+            languages: true,
+        },
     })
     res.json(movies)
 })
 
-app.listen(port, ()=>{
+app.post("/movies", async (req, res) => {
+    await prisma.movie.create({
+        data: {
+            title: "Filme de teste",
+            genre_id: 7,
+            language_id: 1,
+            oscar_count: 0,
+            release_date: new Date(2020,0,1)
+        }
+    })
+    res.status(201).send()
+})
+
+app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`)
 })
